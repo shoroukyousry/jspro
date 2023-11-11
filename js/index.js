fa89355fbed0f8cf26c894c6e8ddb83202ceec00
unknown <Shorouk Yossry>
2023-11-11 18:45:37 +0200

    jspro

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ let myName = document.querySelector(".myname");
 let auth = document.querySelector(".left");
 let out = document.querySelector("#out");
 let products = document.querySelector(".products");
+let slide = document.querySelector("#slide");
 
 let user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
@@ -17,30 +18,34 @@ if (user) {
 let product = [
   {
     id: 1,
-    size: "larg",
-    price: "00$",
+    name: "back page",
+    size: "smal",
+    price: "40$",
     imgeurl: "img/img2.jpg",
   },
 
   {
     id: 2,
+    name: "page",
     size: "larg",
-    price: "60$",
-    imgeurl: "img/img2.jpg",
+    price: "100$",
+    imgeurl: "img/img3.jpg",
   },
 
   {
     id: 3,
-    size: "larg",
+    name: "sheose",
+    size: "32",
     price: "60$",
-    imgeurl: "img/img2.jpg",
+    imgeurl: "img/img4.jpg",
   },
 
   {
     id: 4,
-    size: "larg",
-    price: "60$",
-    imgeurl: "img/img2.jpg",
+    name: "sheose plack",
+    size: "40",
+    price: "160$",
+    imgeurl: "img/img5.jpg",
   },
 ];
 
@@ -54,7 +59,7 @@ function drawproductsui() {
 
           <div class="disc-pro-1">
             <ul>
-              
+              <li>name:${item.name}<li>
               <li>size:${item.size}</li>
               <li>price:${item.price}</li>
             </ul>
@@ -62,7 +67,7 @@ function drawproductsui() {
 
           <div class="icon">
             <ul>
-              <li><button>add to car</button></li>
+              <li><button onclick="addShoppingCar()">add to car</button></li>
               <li><i class="fa-solid fa-heart"></i></li>
             </ul>
           </div>
@@ -70,6 +75,21 @@ function drawproductsui() {
         </div>
     `;
   });
-   products.innerHTML = productsui;
-};
+  products.innerHTML = productsui;
+}
 drawproductsui();
+
+function addedToCar(id) {
+  let shosenItem = product.find((item) => item.id === id);
+  slide.innerHTML += `<p>${shosenItem.titel}</p>`;
+}
+
+function addShoppingCar() {
+  let ss = JSON.parse(localStorage.getItem("userData"));
+  console.log(ss.userName);
+  if (ss.userName) {
+    Window.location = "shoppingcar.html";
+  } else {
+    window.location = "login.html";
+  }
+}
